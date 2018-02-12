28ee747f13bef360ee3cdffe04b39c485777985c
jaspreetsingh3305 <jaspreetsingh3305@gmail.com>
2018-02-11 19:25:29 -0500

    lab 1

---
+++ lab1/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(String product)
+        {
+           
+            ViewBag.passedProduct = product;
+            return View(ViewBag);
+        }
+    }
+}
